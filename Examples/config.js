e848b5afe6a4e0dd056e4d739bccf1631e093db0
Shashank-Ippagunta <150219642+Shashank-Ippagunta@users.noreply.github.com>
2023-11-17 17:53:05 +0530

    Add files via upload

---
+++ Examples/config.js
@@ -0,0 +1,4 @@
+var config = {
+    accessToken: 'pk.eyJ1Ijoic2hhc2hhbmtpcHBhZ3VudGEiLCJhIjoiY2t6MnE5cjRnMDBkdzJ1azJzbjRramJrOSJ9.7G5aAE5xzTFbCPXXjSH5DA',
+    subscriptionKey: 'azure maps subscription key goes here'
+};
